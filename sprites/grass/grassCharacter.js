6e6a525f14ee625c299cbd7354c3832e020bc710
Eric <bookjunkie93@gmail.com>
2017-01-18 10:08:10 -0500

    food sprites can now adjust color

--- sprites/grass/grassCharacter.js
+++ sprites/grass/grassCharacter.js
@@ -6,13 +6,25 @@ const grassCharacter = {
   animationTypes: ['IDLE', 'ALL'],
   frames:[
     require("./grass.png"),
+    require("./grass_blue.png"),
+    require("./grass_green.png"),
+    require("./grass_red.png"),
+    require("./grass_yellow.png"),
   ],
   animationIndex: function getAnimationIndex (animationType) {
     switch (animationType) {
       case 'IDLE':
         return [0];
+      case 'BLUE':
+        return [1];
+      case 'GREEN':
+        return [2];
+      case 'RED':
+        return [3];
+      case 'YELLOW':
+        return [4];
       case 'ALL':
-        return [0];
+        return [0,1,2,3,4];
     }
   },
 };
