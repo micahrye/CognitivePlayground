9ee32b109545fa4da42dd260c51e8c43d862d2b0
rye <micahrye@stardust.local>
2017-03-26 00:36:40 -0400

    added ambient sound to BugZapRedesign

--- Games/BugZap/BugZapRedesign.js
+++ Games/BugZap/BugZapRedesign.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Image,
   View,
+  AppState,
 } from 'react-native';
 
 // NOTES for myself to look back on as I continue to redo things
@@ -25,12 +26,16 @@ import lever from "../../sprites/verticalLever/verticalLeverCharacter";
 import styles from "./BugZapStyles";
 import gameUtil from './gameUtil';
 
+const Sound = require('react-native-sound');
+
 const SCREEN_WIDTH = require('Dimensions').get('window').width;
 const SCREEN_HEIGHT = require('Dimensions').get('window').height;
 
 class BugZapGameRedesign extends React.Component {
   constructor (props) {
     super(props);
+    // BUG: state is dependent on activeFrogColor, if order changed then we
+    // throw an exception.
     this.trialNumber = 1;
     this.activeFrogColor = blueFrogCharacter;
     this.showOtherSign = false;
@@ -41,6 +46,7 @@ class BugZapGameRedesign extends React.Component {
     this.trialOver = false;
     this.retractingSign = true;
     this.leverPressable = true;
+
     this.state = {
       loadingScreen: true,
       leverAnimationIndex: lever.animationIndex('IDLE'),
@@ -53,18 +59,113 @@ class BugZapGameRedesign extends React.Component {
       showFrog: false,
       showSplashCharacter: false,
     };
+
+    this.ambientSound;
+    this.signSound;
+    this.signSoundPlaying = false;
+    this.popSound;
+    this.popPlaying = false;
+    this.celebrateSound;
+    this.celebratePlaying = false;
+    this.disgustSound;
+    this.disgustPlaying = false;
   }
 
   componentWillMount () {
+
   }
 
   componentDidMount () {
+    this.ambientSound = new Sound('ambient_swamp.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.warn('failed to load the sound', error);
+        return;
+      }
+      this.ambientSound.setSpeed(1);
+      this.ambientSound.setNumberOfLoops(-1);
+      this.ambientSound.play();
+      this.ambientSound.setVolume(1);
+    });
+    this.initSounds();
+    AppState.addEventListener('change', this._handleAppStateChange);
   }
 
   componentWillUnmount () {
-    this.clearTimeout(this.leverInterval);
-    this.clearTimeout(this.eatDelay);
-    this.clearTimeout(this.clearScene);
+    this.releaseSounds();
+    clearTimeout(this.leverInterval);
+    clearTimeout(this.eatDelay);
+    clearTimeout(this.clearScene);
+  }
+
+  initSounds () {
+    this.signSound = new Sound('cards_drop.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.warn('failed to load the sound', error);
+        return;
+      }
+      this.signSound.setSpeed(1);
+      this.signSound.setNumberOfLoops(0);
+      this.signSound.setVolume(1);
+    });
+    this.popSound = new Sound('pop_touch.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.warn('failed to load the sound', error);
+        return;
+      }
+      this.popSound.setSpeed(1);
+      this.popSound.setNumberOfLoops(0);
+      this.popSound.setVolume(1);
+    });
+    this.leverSound = new Sound('lever_switch.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.warn('failed to load the sound', error);
+        return;
+      }
+      this.leverSound.setSpeed(1);
+      this.leverSound.setNumberOfLoops(0);
+      this.leverSound.setVolume(1);
+    });
+    this.celebrateSound = new Sound('celebrate.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.warn('failed to load the sound', error);
+        return;
+      }
+      this.celebrateSound.setSpeed(1);
+      this.celebrateSound.setNumberOfLoops(0);
+      this.celebrateSound.setVolume(1);
+    });
+    this.disgustSound = new Sound('disgust.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        console.warn('failed to load the sound', error);
+        return;
+      }
+      this.disgustSound.setSpeed(1);
+      this.disgustSound.setNumberOfLoops(0);
+      this.disgustSound.setVolume(0.9);
+    });
+  }
+
+  releaseSounds () {
+    this.ambientSound.stop();
+    this.ambientSound.release();
+    this.popSound.stop();
+    this.popSound.release();
+    this.leverSound.stop();
+    this.leverSound.release();
+    this.signSound.stop();
+    this.signSound.release();
+    this.celebrateSound.stop();
+    this.celebrateSound.release();
+    this.disgustSound.stop();
+    this.disgustSound.release();
+  }
+
+  _handleAppStateChange = (appState) => {
+    // release all sound objects
+    if (appState === 'inactive' || appState === 'background') {
+      this.releaseSounds();
+      AppState.removeEventListener('change', this._handleAppStateChange);
+    }
   }
 
   onLoadScreenFinish () {
@@ -72,15 +173,16 @@ class BugZapGameRedesign extends React.Component {
   }
 
   setCharacterDirection () {
-    let whichFrog = Math.floor(Math.random() * 2);
-
+    // BUG: currently only chooses one direction. 
+    let whichFrog = Math.random();
+    console.log(`whichFrog = ${whichFrog}`);
     if (whichFrog >= 0.5) {
       this.activeFrogColor = greenFrogCharacter;
       this.frogPosX = 10 * this.props.scale.screenWidth;
       this.frogSide = 'left';
       this.splashPosX = this.frogPosX;
       this.rotate = [{rotateY: '180deg'}];
-   }
+    }
   }
 
   leverPressIn () {
@@ -88,7 +190,10 @@ class BugZapGameRedesign extends React.Component {
       this.setCharacterDirection();
       this.showOtherSign = true;
     }
-
+    if (!this.leverPlaying) {
+      this.leverPlaying = true;
+      this.leverSound.play(() => {this.leverPlaying = false;});
+    }
     if (this.leverPressable) {
       this.setState({
         leverAnimationIndex: lever.animationIndex('SWITCH_ON'),
@@ -108,12 +213,17 @@ class BugZapGameRedesign extends React.Component {
     }
 
     clearTimeout(this.leverInterval); // if finger up before timeout complete
+
     this.setState({
       leverAnimationIndex: lever.animationIndex('SWITCH_OFF'),
     });
   }
 
   signDown () {
+    if (!this.signSoundPlaying) {
+      this.signSoundPlaying = true;
+      this.signSound.play(() => {this.signSoundPlaying = false;});
+    }
     this.retractingSign = false;
     let signTweenOptions =
       gameUtil.getTweenOptions('sign', 'on', this.props.scale.image,
@@ -122,11 +232,12 @@ class BugZapGameRedesign extends React.Component {
     this.setState({
       signRightTweenOptions: signTweenOptions,
       signLeftTweenOptions: signTweenOptions,
-    }, () => { this.refs.signRightRef.spriteTween();
-               if (this.showOtherSign) {
-                 this.refs.signLeftRef.spriteTween();
-               }
-             });
+    }, () => {
+      this.refs.signRightRef.startTween();
+      if (this.showOtherSign) {
+        this.refs.signLeftRef.startTween();
+      }
+    });
   }
 
   retractSign () {
@@ -142,9 +253,9 @@ class BugZapGameRedesign extends React.Component {
     this.setState({
         signRightTweenOptions: signRightTweenOptions,
         signLeftTweenOptions: signLeftTweenOptions,
-    }, () => { this.refs.signRightRef.spriteTween();
+    }, () => { this.refs.signRightRef.startTween();
                if (this.showOtherSign) {
-                 this.refs.signLeftRef.spriteTween();
+                 this.refs.signLeftRef.startTween();
                }
              });
   }
@@ -167,10 +278,10 @@ class BugZapGameRedesign extends React.Component {
 
   correctBugTapped (bugSide) {
     if (bugSide == 'left') {
-      this.refs.bugLeftRef.spriteTween();
+      this.refs.bugLeftRef.startTween();
     }
     else {
-      this.refs.bugRightRef.spriteTween();
+      this.refs.bugRightRef.startTween();
     }
     let delay = 700;
     this.eatDelay = setTimeout (() => {
@@ -258,18 +369,6 @@ class BugZapGameRedesign extends React.Component {
           onPressOut={() => this.leverPressOut()}
         />
 
-        {this.state.showFrog ?
-          <AnimatedSprite
-            spriteUID={'frog'}
-            sprite={this.activeFrogColor}
-            coordinates={{top: 300 * this.props.scale.screenHeight, left: this.frogPosX}}
-            size={gameUtil.getSize('frog', this.props.scale.image)}
-            animationFrameIndex={this.state.frogAnimationIndex}
-            rotate={this.rotate}
-            onAnimationFinish={(characterUID) => this.onAnimationFinish(characterUID)}
-          />
-        : null}
-
         {this.state.showSplashCharacter ?
           <AnimatedSprite
             spriteUID={'splash'}
@@ -278,6 +377,18 @@ class BugZapGameRedesign extends React.Component {
               left: this.splashPosX}}
             size={gameUtil.getSize('splash', this.props.scale.image)}
             animationFrameIndex={this.state.splashAnimationIndex}
+            onAnimationFinish={(characterUID) => this.onAnimationFinish(characterUID)}
+          />
+        : null}
+
+        {this.state.showFrog ?
+          <AnimatedSprite
+            spriteUID={'frog'}
+            sprite={this.activeFrogColor}
+            coordinates={{top: 300 * this.props.scale.screenHeight, left: this.frogPosX}}
+            size={gameUtil.getSize('frog', this.props.scale.image)}
+            animationFrameIndex={this.state.frogAnimationIndex}
+            rotate={this.rotate}
             onAnimationFinish={(characterUID) => this.onAnimationFinish(characterUID)}
           />
         : null}
@@ -344,24 +455,24 @@ class BugZapGameRedesign extends React.Component {
               /> : null}
           </View>: null}
 
-
-        {this.state.loadingScreen ?
-          <LoadScreen
-            onTweenFinish={() => this.onLoadScreenFinish()}
-            width={SCREEN_WIDTH}
-            height={SCREEN_HEIGHT}
+          <HomeButton
+            route={this.props.route}
+            navigator={this.props.navigator}
+            routeId={{ id: 'Main' }}
+            styles={{
+              width: 150 * this.props.scale.image,
+              height: 150 * this.props.scale.image,
+              top:0, left: 0, position: 'absolute' }}
           />
-        : null}
 
-        <HomeButton
-          route={this.props.route}
-          navigator={this.props.navigator}
-          routeId={{ id: 'Main' }}
-          styles={{
-            width: 150 * this.props.scale.image,
-            height: 150 * this.props.scale.image,
-            top:0, left: 0, position: 'absolute' }}
-        />
+          {this.state.loadingScreen ?
+            <LoadScreen
+              onTweenFinish={() => this.onLoadScreenFinish()}
+              width={SCREEN_WIDTH}
+              height={SCREEN_HEIGHT}
+            />
+          : null}
+
       </Image>
     );
   }
